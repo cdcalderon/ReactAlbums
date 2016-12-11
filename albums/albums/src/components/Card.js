506ce27f877e1b2863d8036acf7d64da2b2d8b27
carlos calderon <carloscalderon@carloss-MacBook-Pro-2.local>
2016-12-11 15:34:56 -0600

    add Album Image and re-layout main view

--- albums/albums/src/components/Card.js
+++ albums/albums/src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <View style={styles.containerStyle}>
+            {props.children}
         </View>
     );
 };
@@ -21,7 +22,8 @@ const styles = {
         elevation: 1,
         marginLeft: 5,
         marginRight: 5,
-        marginTop: 10
+        marginTop: 10,
+        backgroundColor: 'yellow'
     }
 };
 
