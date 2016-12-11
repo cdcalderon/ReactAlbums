982a85649666316621d1a1d4f43e7e74a9c2bda4
carlos calderon <carloscalderon@carloss-MacBook-Pro-2.local>
2016-12-11 11:25:28 -0600

    initial commit

---
+++ albums/albums/index.ios.js
@@ -0,0 +1,21 @@
+// Index.ios.js - place code in here for ios
+// http://rallycoding.herokuapp.com/api/music_albums
+// title, artist, url, image, thumnail_image
+
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+const App = () => (
+    <View>
+         <Header headerText={'Albums'} />
+         <AlbumList />
+    </View>
+);
+
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
